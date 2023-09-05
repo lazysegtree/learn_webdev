6d21de3107c920763c2e7742dc430960523c2daf
Nitin Kumar <nitin.l.kumar@oracle.com>
2023-09-05 19:25:47 +0530

    added delay param in tcp servers

--- python/socket/tcp_hello.py
+++ python/socket/tcp_hello.py
@@ -1,12 +1,30 @@
 import socket 
 import sys 
 import random
+import time
 
-if len(sys.argv) == 1 :
-    print("Need to specify a port")
+if len(sys.argv) != 5 :
+    print("Need to specify a port and delay. Example `python3 server.py -p 8080 -d 1`")
     sys.exit(1)
 
-server_port = int(sys.argv[1])
+server_port = -1
+delay = 0
+idx = 1
+
+while idx<len(sys.argv) :
+    if sys.argv[idx] == "-p":
+        server_port = int(sys.argv[idx+1])
+        idx += 2
+    elif sys.argv[idx] == "-d":
+        delay = float(sys.argv[idx+1])
+        idx += 2
+    else:
+        print("invalid args")
+        sys.exit(3)
+
+if (server_port == -1) :
+    print("Invalid args.")
+
 server_ip = "127.0.0.1"
 server_addr = (server_ip, server_port)
 
@@ -23,6 +41,9 @@ while True:
     print(f"Accepted from {client_socket},{client_address}")
     data = client_socket.recv(10)
     print(f"Data recieved : {data}")
+
+    time.sleep(delay);
+
     res = "Hello from " + str(server_num)
     client_socket.send(res.encode())
 
